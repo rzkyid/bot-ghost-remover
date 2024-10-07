0eb29af49dea56aea036a8e48b5163b88445c674
rzkyid <66782146+rzkyid@users.noreply.github.com>
2024-10-07 18:32:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["⚠️ Mohon Perhatian", "👥 Bagi Seluruh Warga", "📝 Baca Peraturan Desa!"];
+const statusMessages = ["⚠️Mohon Perhatian", "👥Bagi Seluruh Warga", "📝Baca Peraturan Desa!"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
